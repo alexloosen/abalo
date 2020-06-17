d2205c9b8082ac7c5ad0fb52384ba832995301f4
Irene <irene.ackermann@alumni.fh-aachen.de>
2020-06-17 11:22:14 +0200

    m5 aufg 2 and 3

--- public/js/newsite/siteheader.js
+++ public/js/newsite/siteheader.js
@@ -7,14 +7,40 @@ Vue.component('siteheader',{
         '                    <div class="col">\n' +
         '                        <h1>Abalo</h1>\n' +
         '                        <p>Das ist unser Slogan.</p>\n' +
-        '                    </div>\n' +
+        '                    </div>' +
+        '                    <div id="message-box" class="col m-3" style="background-color: indianred; text-align: center;"></div>\n' +
         '                </div>\n' +
         '            </div>\n' +
         '        </div>\n' +
         '    </header>',
+    created: function() {
+        let serverUrl = 'ws://127.0.0.1:8001/demo';
+        let socket = new WebSocket(serverUrl);
+        socket.onopen = (msg) => {
+            console.log('Connected');
+        };
+        socket.onmessage = (msg) => {
+            console.log('Message received: ' + JSON.parse(msg.data).data);
+            let tmp = document.getElementById('message-box');
+            tmp.setAttribute('class', 'col-2 m-3');
+            tmp.setAttribute('style', 'background-color: indianred; text-align:center;');
+            tmp.innerHTML = JSON.parse(msg.data).data + '<p></p>';
+            let tmp_button = document.createElement('button');
+            tmp_button.setAttribute('onclick', 'noHTML');
+            let symbol = document.createElement('i');
+            symbol.setAttribute('class', 'fa fa-times');
+            symbol.setAttribute('aria-hidden', 'true');
+            tmp_button.appendChild(symbol);
+            tmp.appendChild(tmp_button);
+        };
+        socket.onclose = (msg) => {
+            console.log('Connection was closed.');
+        };
+    },
     methods: {
         menuHandling: function () {
             this.$emit('menuevent', 'menu-component');
         }
     }
 });
+
